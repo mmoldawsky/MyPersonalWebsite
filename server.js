0a2a50764d7b71eebdc266f5fc2213c769a24fc4
mmoldawsky <mattmoldawsky@gmail.com>
2026-08-12 15:55:13 -0400

    Update .env.example instructions and modify server.js to listen on all interfaces

--- server.js
+++ server.js
@@ -26,4 +26,4 @@ app.get('/api/weather', async (req, res) => {
   }
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => console.log(`Server running on port ${PORT}`));
